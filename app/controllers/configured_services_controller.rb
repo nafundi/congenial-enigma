8e867bd802f027331b24f6dd685760c6511519b2
Matthew White <matthew-white@users.noreply.github.com>
2017-05-04 13:37:07 -0400

    Add configured services controller and views

---
+++ app/controllers/configured_services_controller.rb
@@ -0,0 +1,65 @@
+class ConfiguredServicesController < ApplicationController
+  before_action :set_service, only: [:edit, :update, :destroy]
+
+  add_breadcrumb 'My Servers', :configured_services_path
+
+  def new
+    @service = ConfiguredService.new
+    render_new
+  end
+
+  def create
+    @service = ConfiguredService.new(service_params)
+    if @service.save
+      redirect_to configured_services_path
+    else
+      render_new
+    end
+  end
+
+  def index
+    @services = ConfiguredService.order_by_name.load
+  end
+
+  def edit
+    render_edit
+  end
+
+  def update
+    if @service.update(service_params)
+      redirect_to configured_services_path
+    else
+      render_edit
+    end
+  end
+
+  def destroy
+    @service.destroy
+    redirect_to configured_services_path
+  end
+
+  protected
+
+  def set_service
+    @service = ConfiguredService.find(params[:id])
+  end
+
+  def render_new
+    add_breadcrumb 'Add Server'
+    render 'new'
+  end
+
+  def service_params
+    unless ConfiguredService.type_class_names.include? params[:type]
+      raise ArgumentError
+    end
+    service_class = params[:type].constantize
+    settings = service_class.supported_settings.map(&:to_sym)
+    params.permit(:type, :name, settings: settings)
+  end
+
+  def render_edit
+    add_breadcrumb @service.name
+    render 'edit'
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,13 +3,15 @@ Rails.application.routes.draw do
 
   root 'data_sources#index'
 
-  resources :data_sources, except: :show do
-    member do
-      # This is the route for data sources pushing to the app, not for the app
-      # itself pushing.
-      post 'push'
-    end
+  resources :configured_services, except: :show do
+    resources :data_sources, except: :show do
+      member do
+        # This is the route for data sources pushing to the app, not for the app
+        # itself pushing.
+        post 'push'
+      end
 
-    resources :alerts, except: :show
+      resources :alerts, except: :show
+    end
   end
 end
