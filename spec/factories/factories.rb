508239342b0dea56a71dcbbd2c838c599d92bf60
Matthew White <matthew-white@users.noreply.github.com>
2017-05-04 13:37:07 -0400

    Fix specs

--- spec/factories/factories.rb
+++ spec/factories/factories.rb
@@ -1,11 +1,23 @@
 FactoryGirl.define do
+  factory :configured_service do
+    name 'Test Configured Service'
+  end
+
+  factory :configured_service_odk, class: ConfiguredServices::Odk,
+          parent: :configured_service do
+    settings do
+      { 'url' => 'https://some.url' }
+    end
+  end
+
   factory :data_source do
     name 'Test Data Source'
   end
 
   factory :data_source_odk, class: DataSources::Odk, parent: :data_source do
+    association :configured_service, factory: :configured_service_odk
     settings do
-      { 'url' => 'https://some.url' }
+      { 'form_id' => 'some_id' }
     end
   end
 end
